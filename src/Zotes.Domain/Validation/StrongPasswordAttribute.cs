30e77367c24440b4db34c250a4756c26f58aac04
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-11-05 20:25:52 +0100

    Refactor StrongPasswordAttribute to simplify validation logic and improve readability

--- src/Zotes.Domain/Validation/StrongPasswordAttribute.cs
+++ src/Zotes.Domain/Validation/StrongPasswordAttribute.cs
@@ -8,29 +8,19 @@ public sealed class StrongPasswordAttribute : ValidationAttribute
     protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
         if (value is not string s)
-        {
             return new ValidationResult("Password is required.");
-        }
 
         if (s.Length < 8)
-        {
             return new ValidationResult("Password must be at least 8 characters long.");
-        }
 
         if (!Regex.IsMatch(s, "[A-Z]"))
-        {
             return new ValidationResult("Password must contain at least one uppercase letter.");
-        }
 
         if (!Regex.IsMatch(s, "[a-z]"))
-        {
             return new ValidationResult("Password must contain at least one lowercase letter.");
-        }
 
         if (!Regex.IsMatch(s, "[0-9]"))
-        {
             return new ValidationResult("Password must contain at least one digit.");
-        }
 
         return ValidationResult.Success;
     }
