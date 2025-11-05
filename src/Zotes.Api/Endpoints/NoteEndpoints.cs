7fa260a94c211a701d81a5e9e4fc10ec272acf1c
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-11-05 22:53:45 +0100

    Add 401 Unauthorized response handling to Note endpoints

--- src/Zotes.Api/Endpoints/NoteEndpoints.cs
+++ src/Zotes.Api/Endpoints/NoteEndpoints.cs
@@ -18,18 +18,21 @@ public static class NoteEndpoints
         group.MapGet("", NoteHandlers.GetNotesAsync)
             .WithSummary("Get all Notes")
             .WithDescription("Get all Notes")
-            .Produces<List<NoteDto>>();
+            .Produces<List<NoteDto>>()
+            .Produces(StatusCodes.Status401Unauthorized);
 
         group.MapGet("/{id:guid}", NoteHandlers.GetNoteAsync)
             .WithSummary("Get Note by ID")
             .WithDescription("Get Note by ID")
             .Produces<NoteDto>()
+            .Produces(StatusCodes.Status401Unauthorized)
             .Produces(StatusCodes.Status404NotFound);
 
         group.MapPost("", NoteHandlers.CreateNoteAsync)
             .WithSummary("Create a new Note")
             .WithDescription("Create a new Note")
             .Produces<NoteDto>(StatusCodes.Status201Created)
+            .Produces(StatusCodes.Status401Unauthorized)
             .Produces(StatusCodes.Status400BadRequest);
 
         group.MapPut("/{id:guid}", NoteHandlers.UpdateNoteAsync)
@@ -37,12 +40,14 @@ public static class NoteEndpoints
             .WithDescription("Update a Note by ID")
             .Produces(StatusCodes.Status204NoContent)
             .Produces(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status401Unauthorized)
             .Produces(StatusCodes.Status404NotFound);
 
         group.MapDelete("/{id:guid}", NoteHandlers.DeleteNoteAsync)
             .WithSummary("Delete a Note by ID")
             .WithDescription("Delete a Note by ID")
             .Produces(StatusCodes.Status204NoContent)
+            .Produces(StatusCodes.Status401Unauthorized)
             .Produces(StatusCodes.Status404NotFound);
     }
 }
